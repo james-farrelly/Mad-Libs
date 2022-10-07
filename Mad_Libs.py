e11a70d5fda728f9e5ae2684870e0bc841c2c346
james-farrelly <114578905+james-farrelly@users.noreply.github.com>
2022-10-07 09:27:40 +0100

    Add files via upload

---
+++ Mad_Libs.py
@@ -0,0 +1,18 @@
+place = input("Enter a place name")
+name = input("Enter a name")
+adjective1 = input("Enter an adjective")
+noun1 = input("Enter a noun")
+noun2 = input("Enter a noun")
+adjective2 = input("Enter an adjective")
+adjective3 = input("Enter an adjective")
+noun3 = input("Enter a noun")
+number = input("Enter a number")
+noun4 = input("Enter a noun")
+food = input("Enter a food")
+verb = input("Enter a verb")
+
+mad_lib = ("I went to the beach in " + place + " today with " + name + ". We had a " + adjective1 + " time .\nWe first built a " + noun1 + " and then we ran around in the " + noun2 +
+" for a little while. The water was a bit " + adjective2 + ". \nThe sun was very " + adjective3 + " so we made sure to wear a lot of " + noun3 + ". We spent " + number +
+" hours there.\nAt the end of the day we treated ourselves to a " + noun4 + " and had " + food + " for dinner.\nI definitely want to go to the beach again but next time I want to " + verb)
+
+print(mad_lib)
